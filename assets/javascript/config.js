561f825d9da315914487c0754f0a1a8c6a032d5d
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-03-21 10:31:36 -0400

    Body System Smart Select and Symptom Accordion
    I created a new TESTindex.html view this in browser to see the changes.

---
+++ assets/javascript/config.js
@@ -0,0 +1,29 @@
+// Dom7
+var $$ = Dom7;
+
+// Framework7 App main instance
+var app  = new Framework7({
+    root: '#app', // App root element
+    id: 'io.framework7.testapp', // App bundle ID
+    name: 'Framework7', // App name
+    theme: 'auto', // Automatic theme detection
+    smartSelect: {
+        openIn: 'page',
+        closeOnSelect: true,
+    },
+    // App root methods
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    // App routes
+    routes: routes,
+});
+
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+  url: '/'
+});
+
+    

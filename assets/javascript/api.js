236677bbbb0ee8a89622c18731e87189108d73a5
Unknown <petercarmines@gmail.com>
2018-03-22 21:09:19 -0400

    Auto stash before merge of "Dev" and "origin/Dev"
    Added languages objects to smartselect.js, begin getting form data on api.js, temporary submit button added for testing purposes to index.html

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,44 +1,33 @@
-var $$ = Dom7;
+var formBS = [];
 
-// Framework7 App main instance
-var app  = new Framework7({
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
-  
-  // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
-  },
-  // App routes
-  routes: routes,
-});
-
-// Init/Create main view
-var mainView = app.views.create('.view-main', {
-  url: '/'
-});
-
-$('#submit').on('click', function () {
+$('#submit').on('click', function () { //function that translates when submit is clicked
   event.preventDefault();
   var twoLetterCode = [];
     $$('select[name="languages"] option:checked').each(function () {
       twoLetterCode.push(this.value);
-    });        
+    });     
+
+    
+      var selectedIndex = app.form.convertToData('#myForm')
+      console.log(selectedIndex)
+      for(var propt in selectedIndex){
+        formBS.push(bsIndexArray[selectedIndex[propt]])
+        console.log(formBS)
+    } 
+      var test = selectedIndex
+      
   translatedBS = [];
   $$('select[name="bodysystems"] option:checked').each(function () {
   translatedBS.push(this.text);
   });      
   var yandex = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180315T074722Z.24f814dba1dc1456.f7b63ba7e42f3a418e49b4c076e0902d50b35012&text="
-  + translatedBS + "&lang=" + "en" + "-" + twoLetterCode + "&format=plain"
+  + formBS + "&lang=" + "en" + "-" + "es" + "&format=plain"
     $.ajax({
     url: yandex,
     method: "GET"
     }).then(function(translated) {
     $("#results").html(translated.text)
+    console.log(translated)
     });           
 });
 
